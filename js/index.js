e55344994aa7b68a35be6e6fd37a84c276b75488
Illia Zakharov <illia_zakharov@epam.com>
2022-04-24 00:48:59 +0300

    implemented UI

--- js/index.js
+++ js/index.js
@@ -2,19 +2,65 @@ import { Cezar } from './ecryption-methods/cezar.js';
 import { Polybius } from './ecryption-methods/polibius-square.js';
 import { Vizhener } from './ecryption-methods/vizhener-square.js';
 
-const phrase = 'i remember that september';
+const CIPHERS = {
+    vizhener: {
+        init: (lang) => new Vizhener(lang),
+        changeForm: '',
+    },
+    cezar: {
+        init: (lang) => new Cezar(lang),
+        changeForm: '',
+    },
+    polybius: {
+        init: (lang) => new Polybius(lang),
+        changeForm: '',
+    }
+}
 
-let polybius = new Polybius('en');
+const cipherSelector = document.getElementById('cipher-selector');
+const langSelector = document.getElementById('alphabet-lang');
 
-console.log(polybius.name, polybius.encrypt(phrase));
-console.log(polybius.name, polybius.decrypt(polybius.encrypt(phrase)));
+const inputData = document.getElementById('input-data');
+inputData.addEventListener('input', function () {
+    this.value ? enableBtns() : disableBtns();
+});
 
-let cezar = new Cezar('en');
+/**** [START] BUTTONS ****/
+const clearBtn = document.getElementById('clear');
+clearBtn.addEventListener('click', function () {
+    inputData.value = '';
+    disableBtns();
+});
 
-console.log(cezar.name, cezar.encrypt(phrase));
-console.log(cezar.name, cezar.decrypt(cezar.encrypt(phrase)));
+const decryptBtn = document.getElementById('decrypt');
+decryptBtn.addEventListener('click', function () {
+    let method = getMethod();
 
-let vizhener = new Vizhener('en');
+    if (inputData.value) {
+        alert(method.decrypt(inputData.value));
+    }
+});
 
-console.log(vizhener.name, vizhener.encrypt(phrase, 'leonid'));
-console.log(vizhener.name, vizhener.decrypt(vizhener.encrypt(phrase,'leonid'),'leonid'));
+const encryptBtn = document.getElementById('encrypt');
+encryptBtn.addEventListener('click', function () {
+    let method = getMethod();
+
+    if (inputData.value) {
+        alert(method.encrypt(inputData.value));
+    }
+});
+/**** [END] BUTTONS ****/
+
+function getMethod() {
+    return CIPHERS[cipherSelector.value].init(langSelector.value);
+}
+
+function enableBtns() {
+    encryptBtn.removeAttribute('disabled');
+    decryptBtn.removeAttribute('disabled');
+}
+
+function disableBtns() {
+    encryptBtn.setAttribute('disabled', 'disabled');
+    decryptBtn.setAttribute('disabled', 'disabled');
+}
